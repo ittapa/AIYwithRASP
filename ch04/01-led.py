6e77cdfa2c698c265835fde7f07e6a82c77e0f45
sangguk <ittapa@naver.com>
2019-08-13 03:04:27 +0900

    Create 01-led.py

---
+++ ch04/01-led.py
@@ -0,0 +1,31 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+
+LED=12
+
+GPIO.setup(LED, GPIO.OUT, initial=GPIO.LOW)
+
+print("GPIO.OUT: ", GPIO.OUT)
+print("GPIO.HIGH: ", GPIO.HIGH)
+print("GPIO.LOW: ", GPIO.LOW)
+
+try:
+    while 1:
+        #
+        GPIO.output(LED, GPIO.HIGH)
+
+        #0.5초대기
+        time.sleep(0.5)
+
+        GPIO.output(LED, GPIO.LOW)
+
+        time.sleep(0.5)
+
+except KeyboardInterrupt:
+    print("END")
+    pass
+
+#GPIO 개방
+GPIO.cleanup()
