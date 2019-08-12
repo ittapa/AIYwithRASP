2d24bce865520595c4c9d4f9a512a6456be6a247
sangguk <ittapa@naver.com>
2019-08-13 03:02:35 +0900

    Create 00-servo.c

---
+++ ch04/00-servo.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+#include<wiringPi.h>
+
+#include<softPwm.h>
+
+
+
+#define SERVO 5
+
+
+
+int main()
+{
+
+	if(wiringPiSetup()==-1)return 1;
+
+	softPwmCreate(SERVO,0,200);
+
+
+    softPwmWrite(SERVO,15); //0 degree
+    delay(500);
+	softPwmWrite(SERVO,24); //90 degree
+    delay(500);
+	softPwmWrite(SERVO,5); //-90 degree
+    delay(500);
+	
+	return 0;
+
+}
